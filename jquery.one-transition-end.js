1cce401ea334c8c7bb1d26bf9594da3201452322
Daniel Beeke <daniel@studiofonkel.nl>
2017-09-06 11:44:57 +0200

    Initial commit

---
+++ jquery.one-transition-end.js
@@ -0,0 +1,32 @@
+$.fn.oneTransitionEnd = function(property, callback) {
+    var that = this;
+    var innerCallback = function (event) {
+        if (event.originalEvent.propertyName.substr(- property.length) === property) {
+            callback();
+            that.off(whichTransitionEvent(), innerCallback);
+        }
+    };
+
+    this.on(whichTransitionEvent(), innerCallback);
+
+    return this;
+};
+
+// Function from David Walsh: http://davidwalsh.name/css-animation-callback
+function whichTransitionEvent(){
+    var t,
+        el = document.createElement("fakeelement");
+
+    var transitions = {
+        "transition"      : "transitionend",
+        "OTransition"     : "oTransitionEnd",
+        "MozTransition"   : "transitionend",
+        "WebkitTransition": "webkitTransitionEnd"
+    };
+
+    for (t in transitions){
+        if (el.style[t] !== undefined){
+            return transitions[t];
+        }
+    }
+}
