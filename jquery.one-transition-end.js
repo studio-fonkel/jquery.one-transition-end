add308c4d5a6cc7875bd2e0988269865f9a1e720
Anoesj Sadraee <anoesjsadraee@gmail.com>
2017-12-02 13:37:47 +0100

    Added a promisified version and shorthands$.fn.oTE and $.fn.oTEP

--- jquery.one-transition-end.js
+++ jquery.one-transition-end.js
@@ -1,19 +1,35 @@
 (function ($) {
 
-  var transitionend = whichTransitionEvent();
+  var transitionEndType = whichTransitionEvent();
 
-  $.fn.oneTransitionEnd = function(property, callback) {
+
+  /** --------------------------------------------------------------
+    * $.fn.oneTransitionEnd (shorthand: $.fn.oTE)
+    * Usage:
+
+    // Do something after fading out an element
+    const $element = $('.someElement').first();
+    $element.oTE('opacity', () => {
+      // do something
+    }, parseFloat($element.css('opacity')) === 0).css('opacity', 0);
+    ------------------------------------------------------------- **/
+  $.fn.oneTransitionEnd = function (property, callback, conditionsToSkip) {
+    if (conditionsToSkip === true) {
+      callback();
+      return this;
+    }
+
     var that = this;
 
-    if (transitionend) {
+    if (transitionEndType) {
       var innerCallback = function (event) {
-        if (event.originalEvent.propertyName.substr(- property.length) === property && event.target == that[0]) {
+        if (event.originalEvent.propertyName.substr(-(property.length)) === property && event.target == that[0]) {
           callback();
-          that.off(transitionend, innerCallback);
+          that.off(transitionEndType, innerCallback);
         }
       };
 
-      this.on(transitionend, innerCallback);
+      this.on(transitionEndType, innerCallback);
     }
 
     else {
@@ -23,8 +39,41 @@
     return this;
   };
 
-  // Function from David Walsh: http://davidwalsh.name/css-animation-callback
-  function whichTransitionEvent(){
+
+  /** --------------------------------------------------------------
+    * $.fn.oneTransitionEndPromisified (shorthand: $.fn.oTEP)
+    * Usage:
+
+    // Do something after fading out an element
+    const $element = $('.someElement').first();
+    $element.oTEP('opacity', parseFloat($element.css('opacity')) === 0, () => {
+      $element.css('opacity', 0);
+    }).then(() => {
+      // do something
+    });
+    ------------------------------------------------------------- **/
+  $.fn.oneTransitionEndPromisified = function (property, conditionsToSkip, transitionAction) {
+    var that = this;
+
+    return new Promise(function (resolve) {
+      that.oneTransitionEnd(property, resolve, conditionsToSkip);
+      transitionAction();
+    });
+  };
+
+
+  /** --------------------------------------------------------------
+    * Shorthands
+    ------------------------------------------------------------- **/
+  $.fn.oTE = $.fn.oneTransitionEnd;
+  $.fn.oTEP = $.fn.oneTransitionEndPromisified;
+
+
+  /** --------------------------------------------------------------
+    * Helpers
+    ------------------------------------------------------------- **/
+  // whichTransitionEvent function from David Walsh: http://davidwalsh.name/css-animation-callback
+  function whichTransitionEvent () {
     var t,
         el = document.createElement("fakeelement");
 
